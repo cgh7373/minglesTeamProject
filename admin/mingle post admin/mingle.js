702439b7a8049b6792a98966154a6d9c71b713fc
kimsssang <swk9871@gmail.com>
2024-08-02 16:36:00 +0900

    관리자페이지

---
+++ admin/mingle post admin/mingle.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    $('#homeKey').click(function () {
+        $('#menuBar').animate({width: 'toggle'})
+    })
+
+    $('#menuBack').click(function () {
+        $('#menuBar').animate({width: 'toggle'})
+    })
+
+    $(document).click(function (event) {
+        if (!$(event.target).closest('#menuBar, #homeKey').length) {
+            if ($('#menuBar').is(":visible")) {
+                $('#menuBar').animate({width: 'toggle'});
+            }
+        }
+    });
+    
+})
