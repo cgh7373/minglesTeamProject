87497b7d2010c75a54e89da6dee4fe897ab30002
tak511 <wjdxordms123@naver.com>
2024-08-02 17:53:26 +0900

    0802tak511

--- mingle/게시글관리/mingle.js
+++ mingle/게시글관리/mingle.js
@@ -9,16 +9,14 @@ $(function() {
     $('#menuBack').click(function() {
         $('#menuBar').animate({width:'toggle'})
     })
-
-
-
-
-
-
-
-
-
-
-
-
 })
+
+//체크박스 전체 선택 js
+function selectAll(selectAll)  {
+    const checkboxes = document.getElementsByName('choice')
+        
+    checkboxes.forEach((checkbox)=> 
+    {
+        checkbox.checked = selectAll.checked;
+    })
+}
